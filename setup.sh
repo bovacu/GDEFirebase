6eb2a49c412cbab80b80c2fe8659427663060e76
Borja <vazquezborja@hotmail.es>
2022-02-22 00:17:08 +0100

    Updated setup

--- setup.sh
+++ setup.sh
@@ -1,3 +1,14 @@
-git submodule init
-git submodule update
-cd ../android && ln -s ../ads/firebase_sdk_cpp .
+if ! [ -d firebase_cpp_sdk ]
+then
+    wget https://firebase.google.com/download/cpp?hl=es && unzip cpp?hl=es && rm cpp?hl=es
+fi
+
+if [ -d ../android ]
+then
+    cd ../android
+    set -i -e "s/#GDEFirebase/include(../GDEFirebase/CMakeLists.txt)/g"
+    ln -s ../GDEFirebase/firebase_cpp_sdk .
+fi
+
+cd ../..
+set -i -e "s/#GDEFirebase/include(targets/GDEFirebase/CMakeLists.txt)"
